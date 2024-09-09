b7bf5374fef2cf854d95baff05cee6c055bb902a
Ciro Pupillo <ciropupillo16@gmail.com>
2024-09-09 09:46:09 -0300

    trabahoconreact

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,19 @@
+import { CiShoppingCart } from "react-icons/ci";
+
+const CartWidget = () => {
+
+    return (
+        <div  style={{
+            display: "flex",
+            marginRight: "20px",
+            alignItems: "center",
+            width: "30%",
+            justifyContent: "space-between",
+            }}>
+            <CiShoppingCart size={30}/>
+            1
+            </div>
+    );
+};
+
+export default CartWidget;
